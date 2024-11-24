13b6ecd079229c6397f6f86304f65425178962f4
Marcelo Antonio <maeceloacm1998@gmail.com>
2024-11-23 23:53:20 -0300

    first commit!

---
+++ src/test/kotlin/marcelodev/comu_carona/ComuCaronaApplicationTests.kt
@@ -0,0 +1,13 @@
+package marcelodev.comu_carona
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComuCaronaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
